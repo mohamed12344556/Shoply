728b08dd34ac53f6c5299c5256709cbd8c944d10
mohamed12344556 <ma5123@fayoum.edu.eg>
2024-10-28 00:22:41 +0300

    2

---
+++ lib/modules/Cart_Payment/Cubit/main_state.dart
@@ -0,0 +1,12 @@
+abstract class mainState {}
+
+class mainInitial extends mainState {}
+
+class CheckOutScreenState extends mainState {}
+class ShoppingPagScreenState extends mainState {}
+class ShoppingPaymentScreenState extends mainState {}
+class BackState extends mainState {}
+class IncPageState extends mainState {}
+class PagechangeState extends mainState {}
+
+
